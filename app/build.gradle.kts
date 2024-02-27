4c8bc5c343c8fa1ab16ec15426a5f158e46d1d03
Chidiebube <ebubeokebalama@gmail.com>
2024-02-27 13:14:31 -0500

    Added the Firebase(google) SDK and dependencies for the app. Made sure dependencies were running at the latest version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-database:20.3.0")
     testImplementation("junit:junit:4.13.2")

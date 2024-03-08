5c75b49d4df2fb19e89b63134aca3acd9e2a10b0
Alexis Pardo <alexispardo27@gmail.com>
2024-03-07 21:54:56 -0500

    Updates StepsFragment.java

--- app/src/main/java/com/example/fitnesstracker/MainActivity.java
+++ app/src/main/java/com/example/fitnesstracker/MainActivity.java
@@ -21,17 +21,9 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity{
 
     ActivityMainBinding binding;
-
-    private SensorManager sensManager = null;
-    private Sensor stepSensor;
-    private int totalSteps = 0;
-    private int preTotalSteps = 0;
-    private ProgressBar progressBar;
-    private TextView stepText;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,14 +48,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             }
             return true;
         });
-
-        progressBar = findViewById(R.id.progressBar);
-        stepText = findViewById(R.id.stepsText);
-
-        resetSteps();
-        loadData();
-        sensManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        stepSensor = sensManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
     }
 
     private void replaceFragment(Fragment fragment){
@@ -72,76 +56,4 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
     }
-
-
-    protected void onResume(){
-        super.onResume();
-
-        if(stepSensor == null){
-            Toast.makeText(this, "Device has no sensor", Toast.LENGTH_SHORT).show();
-        }
-        else {
-            sensManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        }
-    }
-
-    protected void onPause(){
-        super.onPause();
-        sensManager.unregisterListener(this);
-    }
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if(event.sensor.getType() == Sensor.TYPE_STEP_COUNTER){
-            totalSteps = (int) event.values[0];
-            int currentSteps = totalSteps - preTotalSteps;
-            stepText.setText(String.valueOf(currentSteps));
-
-            progressBar.setProgress(currentSteps);
-        }
-    }
-
-    //Mainly for testing
-    private void resetSteps(){
-        if (stepText != null) {
-            stepText.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(MainActivity.this, "long hold to rest", Toast.LENGTH_SHORT).show();
-                }
-            });
-
-            stepText.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View v) {
-                    preTotalSteps = totalSteps;
-                    stepText.setText("0");
-                    progressBar.setProgress(0);
-                    saveData();
-                    return true;
-                }
-            });
-        } else {
-            // Log an error or display a message to help identify the issue
-            Toast.makeText(this, "stepsText is null", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private void saveData(){
-        SharedPreferences sharedPref = getSharedPreferences("myPref", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putString("key1", String.valueOf(preTotalSteps));
-        editor.apply();
-    }
-
-    private void loadData(){
-        SharedPreferences sharedPref = getSharedPreferences("myPref", Context.MODE_PRIVATE);
-        int savedNum = (int) sharedPref.getFloat("key1", 0f);
-        preTotalSteps = savedNum;
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
-
 }

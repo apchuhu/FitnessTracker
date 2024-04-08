fae4731879a6083d2f8adf9130b66044b22b136e
Alexis Pardo <alexispardo27@gmail.com>
2024-04-08 00:16:22 -0400

    Adds UserInput

--- app/src/main/java/com/example/fitnesstracker/TrackFragment.java
+++ app/src/main/java/com/example/fitnesstracker/TrackFragment.java
@@ -1,13 +1,20 @@
 package com.example.fitnesstracker;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -16,7 +23,7 @@ import com.google.firebase.database.FirebaseDatabase;
  * Use the {@link TrackFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TrackFragment extends Fragment {
+public class TrackFragment extends Fragment implements UserInputFragment.EntryInputListener{
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,6 +33,10 @@ public class TrackFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private LinearLayout entriesLayout;
+    private EditText entryEditText;
+    private int entryCount = 0;
 
     public TrackFragment() {
         // Required empty public constructor
@@ -49,6 +60,7 @@ public class TrackFragment extends Fragment {
         return fragment;
     }
 
+    @SuppressLint("WrongViewCast")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,10 +70,43 @@ public class TrackFragment extends Fragment {
         }
     }
 
+    public void addEntry(String entryText) {
+        if (entryText.isEmpty()) {
+            return;
+        }
+        entryCount++;
+        EditText newEntry = new EditText(getContext());
+        newEntry.setText(entryText);
+        newEntry.setEnabled(false);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(8, 8, 8, 8);
+        newEntry.setLayoutParams(layoutParams);
+        entriesLayout.addView(newEntry);
+        entryEditText.setText("");
+    }
+
+    @SuppressLint("WrongViewCast")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_track, container, false);
+        View view = inflater.inflate(R.layout.fragment_track, container, false);
+        entriesLayout = view.findViewById(R.id.entries_layout);
+        entryEditText = view.findViewById(R.id.entry_edit);
+        FloatingActionButton fab = view.findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addEntry(entryEditText.getText().toString());
+            }
+        });
+        return view;
+    }
+
+    private void showEntryInputDialog() {
+        UserInputFragment dialogFragment = new UserInputFragment();
+        dialogFragment.setTargetFragment(this, 0);
+        dialogFragment.show(getParentFragmentManager(), "EntryInputDialogFragment");
     }
 }
